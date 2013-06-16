9d22699f609e776e5132ab6774caa9756c854ad3
shura <shura1991@gmail.com>
2013-06-16 08:50:33 +0200

    better test

--- test.erl
+++ test.erl
@@ -3,6 +3,12 @@
 
 main(_) ->
   X = ervideo:open(0, 640, 480, mmap),
+  X:capture(),
+  {png, _} = X:read(png),
+  X:uncapture(),
+  X:capture(),
+  {png, _} = X:read(png),
+  timer:sleep(1000),
   {png, Bin} = X:read(png),
   ervideo:close(X),
   {ok, File} = file:open("test.png", [write, raw]),
